007ed7739e0871093fccce722d9ae28d9cb3ac1a
Caleb Templer <ctempler@uark.edu>
2026-03-05 13:55:06 -0600

    second commit

---
+++ python_scripts/Hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# prompt user for name
+name = input('please enter your name: ')
+
+# ways to print text
+print('hello world')
+print("hello world")
+
+# output text based on name input
+print('hello, ', name, ' I hope you have a great day.')
+
+# some math
+print(7+7)
+print(7+7*9)
